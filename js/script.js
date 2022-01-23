558fb65fc14fcb64a69ecb57d9b8dad1dc2bb07e
Abdulaziz2004 <abu556626@gmail.com>
2022-01-23 21:38:36 +0500

    loader push

--- js/script.js
+++ js/script.js
@@ -6,3 +6,14 @@ $(document).ready(function () {
     loop: true,
   });
 });
+
+window.addEventListener("DOMContentLoaded", () => {
+  const loader = document.querySelector(".loader");
+
+  setTimeout(() => {
+    loader.style.opacity = "0";
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 5000);
+  }, 2000);
+});
